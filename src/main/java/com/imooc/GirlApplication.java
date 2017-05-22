f788013bd37dedc324162a22f50193292aa1a4a4
unknown <xiao.kailin@ucsmy.com>
2017-05-22 18:23:10 +0800

    测试提交后显示的用户名

--- src/main/java/com/imooc/GirlApplication.java
+++ src/main/java/com/imooc/GirlApplication.java
@@ -9,6 +9,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class GirlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GirlApplication.class, args);
+		SpringApplication.run(GirlApplication.class, args); 
 	}
 }
